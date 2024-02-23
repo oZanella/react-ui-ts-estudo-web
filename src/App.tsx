d9684dad64d7d2397365085bd3efa5593d0f48a2
oZanella <henriquezanella19@gmail.com>
2024-02-22 21:37:33 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
     <BrowserRouter>
-    <div className="App">
-      Teste
-    </div>
+    <AppRoutes />
     </BrowserRouter>
   );
 }
