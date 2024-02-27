355ea8aedd69b2abab88d47081bf93ef286cb8ac
oZanella <henriquezanella19@gmail.com>
2024-02-26 21:24:13 -0300

    primeiro botão inserido

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { LigthTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-    <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LigthTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
