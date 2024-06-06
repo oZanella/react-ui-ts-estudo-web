e2d37e88dcff69ce254ab8e3a2322589570b4064
oZanella <henriquezanella19@gmail.com>
2024-06-05 21:31:44 -0300

    inserido o botão troca de theme part one

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme, useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
-import { useDrawerContext } from '../../contexts';
+import { useAppThemeContext, useDrawerContext } from '../../contexts';
 import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 
 interface IListItemLinkProps {
@@ -44,6 +44,7 @@ export const MenuLateral: React.FC<MenuLateralProps> = ({ children }) => {
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
   const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext();
+  const { toggleTheme } = useAppThemeContext();
 
   return (
     <>
@@ -54,7 +55,7 @@ export const MenuLateral: React.FC<MenuLateralProps> = ({ children }) => {
           <Box width='100%' height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
             <Avatar
               sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
-              src="https://th.bing.com/th/id/OIG3.FNNgd1.IQjV0.YeeexxC?w=1024&h=1024&rs=1&pid=ImgDetMain"
+              src="https://lh3.googleusercontent.com/pw/AP1GczPAq2xctv5RaCC0WeokUCRC0mgZ6eCZwZGTutjEecBx5txZLo1WE_vEDfrpbn-vPgmE54WKbZ017If3xbJam1hwyzvN0etI3EsTXaQfBQJqI6w8SHbMletz9_Zzsooa1mmgA4zqX9fo4fO7b2-lm7ymDf8rKuy2AbeffqP5qot8fIXVPz46khxzPg93JGwQKz9wK1uCwhKFPyl9g-nS7rVGNE9dGHo_cwQN4seXl08_tN1DuUrM88s2VZTbL9TF3hh2IOMWrJp6NnHmC0xAAydHwsYW_6uHfwMJy3qtgCC2qeiKz8mrXbMuOEMwypxY8yoQoDno-V0wb6GSw-8Rdwdby3UnGFqXfB71HL_xpAS_ADQh8yhxunK7sCXsPupl4RYqXx5_DvdoTHMT67XOM7fjVDbj4LLc8PmLyaujNrNHjtJYfKQ7Q5CKFqYbBRUlNIre7tN1MPBueDiXzD6mmOAOCbv8tJhTunV3m427VYTVcWCbzDWDrVczHJ78faVkQVUuL0OGVEfuedutupQmk_SC505yp_u6arVF1EmOZBxy5lTKJKmNY2Qzdj6vGviv3vmpJH65CftgYwEPUL6V3VvMgJ0DeV5Pl-F_XqInFqGp3EbdVkmj00xEoSIwHFiY2iKTUMpQQD1DYnp3Q2H-ilARFnohThlRBe4wHVkQ5fmCdKw-5F7gMB09B6r86MWQQkawXcljW9oFxFNnwWstculGVtVh16TvYZNeQKK7WscwsqiIztgjdp-afhC2UGWNTXWxNmeNrRUwZ1Bpk0efHxyS1vzlGLtd14pcwGH5V8hb-whYiut8xqAdu61UAgQOVp4j4anddgdh-ZUWguZQh2sK8vZ1OMr5KSY3SVnyspn5oWstvbMoUijL0T0CNnQoeJ6Mw6gXg1KDpmRUmRQb6F5iZd8p=w1024-h1024-s-no-gm?authuser=0"
             />
           </Box>
 
@@ -71,6 +72,17 @@ export const MenuLateral: React.FC<MenuLateralProps> = ({ children }) => {
                   onClick={smDown ? toggleDrawerOpen : undefined}
                 />
               ))}
+            </List>
+          </Box>
+
+          <Box>
+            <List component="nav">
+              <ListItemButton onClick={toggleTheme}>
+                <ListItemIcon>
+                  <Icon>dark_mode</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Alterar tema" />
+              </ListItemButton>
             </List>
           </Box>
 
