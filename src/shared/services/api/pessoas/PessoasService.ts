720ff59cabd45da94c4ca162f407604dd6eea92f
oZanella <henriquezanella19@gmail.com>
2024-07-24 19:12:33 -0300

    Inserido erros para console.log, ajustado nome de um componente para useMediaQuery, criado um type new para consultar os dados do back-end e apresentar natela

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -2,14 +2,44 @@ import { Environment } from '../../../environment';
 import { Api } from '../axios-config';
 
 
-const getAll = async (page = 1, filter = ''): Promise<any> => {
+interface IListagemPessoa {
+  id: number;
+  nomeCompleto: string;
+  email: string;
+  cidadeId: number;
+}
+
+interface IDetalhePessoa {
+  id: number;
+  nomeCompleto: string;
+  email: string;
+  cidadeId: number;
+}
+
+type TPessoasComTotalCount = {
+  data: IListagemPessoa[];
+  totalCount: number;
+}
+
+
+const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Error> => {
   try {
     const urlRelativa = `/pessoas?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`;
 
-    const { data } = await Api.get(urlRelativa);
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(headers['x-total-count'] || Environment.LIMITE_DE_LINHAS),
+      };
+    }
+
+    return new Error('Erro ao listar os registros.');
 
   } catch (error) {
-
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao listar os registros.');
   }
 };
 
