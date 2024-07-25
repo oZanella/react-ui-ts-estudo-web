010c92766037f3f7279a8e8fbb5c28104e62a162
oZanella <henriquezanella19@gmail.com>
2024-07-25 08:43:06 -0300

    Finalizado todas as configurações de ações que o usuário pode ou não fazer dentro do cadastro de pessoas,assim foi finalizado o getAll. getByld, create, update Byld e deleteByld

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -43,13 +43,58 @@ const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Er
   }
 };
 
-const getByld = async (): Promise<any> => { };
+const getByld = async (id: number): Promise<IDetalhePessoa | Error> => {     //defini no async que o id=number
+  try {
+    const { data } = await Api.get(`/pessoas/${id}`);   //ajustei a url para pegar o id das pessoas
 
-const create = async (): Promise<any> => { };
+    if (data) {
+      return data;
+    }
 
-const updateByld = async (): Promise<any> => { };
+    return new Error('Erro ao consultar o registro.');
 
-const deleteByld = async (): Promise<any> => { };
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao consultar o registro. ');
+  }
+};
+
+const create = async (dados: Omit<IDetalhePessoa, 'id'>): Promise<number | Error> => //Omit não vai solicitar o Id na hora da busca
+{
+  try {
+    const { data } = await Api.post<IDetalhePessoa>('/pessoas', dados);  //ajustei a url para pegar os dados de IDetalhePessoa e mandar junto com pessoas quando for realizada a busca
+
+    if (data) {
+      return data.id;
+    }
+
+    return new Error('Erro ao criar o registro.');
+
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao criar o registro. ');
+  }
+};
+
+const updateByld = async (id: number, dados: IDetalhePessoa): Promise<void | Error> => { //void significa que não retorna nada
+  try {
+    await Api.put(`/pessoas/${id}`, dados);   //ajustei a url para pegar os dados de IDetalhePessoa e mandar junto com pessoas quando for realizada a busca
+
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao atualizar o registro. ');
+  }
+};
+
+const deleteByld = async (id: number): Promise<void | Error> => {
+  try {
+    await Api.delete(`/pessoas/${id}`);   //Api.delele vai apagar o registro de pessoas que foi criado/tem salvo no banco de dados
+
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao apagar o registro. ');
+  }
+};
 
 
 export const PessoasService = {
